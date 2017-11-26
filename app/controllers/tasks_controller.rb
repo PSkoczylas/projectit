4bf3b980572a96d19ce238f8f5fa7374d2c27b05
PSkoczylas <piotr.skoczylas93@gmail.com>
2017-11-26 21:12:39 +0100

    Add index page to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,10 @@ class TasksController < ApplicationController
   # GET /tasks
   def index
     @tasks = Task.where(user_id: current_user.id, project_id: params[:project_id].to_i)
+    @todo_tasks = @tasks.where(status: :todo)
+    @in_progress_tasks = @tasks.where(status: :in_progress)
+    @code_review_tasks = @tasks.where(status: :code_review)
+    @done_tasks = @tasks.where(status: :done)
   end
 
 end
